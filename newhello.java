88a68782c93af995ccacba380791db7d0d3749be
unknown <puvvadisreevineetha@gmail.com>
2021-09-22 20:53:03 -0700

    Commiting all files

---
+++ newhello.java
@@ -0,0 +1,6 @@
+public class Newhello{
+	public static void main(String args[]){
+		System.out.println("newhello");
+	}
+}
+
---
+++ newhello1.java
@@ -0,0 +1,6 @@
+public class newhello1{
+	public static void main(String args[]){
+		System.out.println("newhello1");
+	}
+}
+
